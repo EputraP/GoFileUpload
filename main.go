ee805be76c35ff9c2eacf1d4d04d590791c51f15
EputraP <EputraP@gmail.com>
2023-05-30 04:52:38 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Uploading File\n")
+	// 1.parse input, type multipart/form data
+	r.ParseMultipartForm(10 << 20)
+	// 2. retrieve file from posted form data
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving file from form-data")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Upload File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("NIME Header: %+v\n", handler.Header)
+	// 3. write temporary file on our server
+	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tempFile.Close()
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tempFile.Write(fileBytes)
+	// 4. return wether or not this has been successful
+	fmt.Fprintf(w, "successfully Uploaded File")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	fmt.Println("Go File Upload Tutorial")
+	setupRoutes()
+}
